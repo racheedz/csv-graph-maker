b9fe35873967e13a47cb4f4c3e643dc0e2a444ec
Racheed <raze757@gmail.com>
2020-08-19 18:38:30 +1000

    Added category creation, plotting with colors and some cleaning

--- csv-graph-maker.py
+++ csv-graph-maker.py
@@ -66,6 +66,9 @@ checks = {}
 short_filename = {}
 flux_dict = {}
 toggler = 2
+category = {}
+category_hue = {}
+
 
 # =============================================================================
 # # -- FUNCTIONS -- #
@@ -77,9 +80,50 @@ def prepare_graph(filename):
     next_data = pd.read_csv(filename)
     graph_files[filename] = next_data
     
+# select a dir, then get every csv file and create a check button for it
+def open_dir():
+    
+    path = filedialog.askdirectory()
+    
+    for root, dirs, files in os.walk(path):
+        
+        for name in files:
+            filename = os.path.join(root, name)
+            filename = filename.replace("\\", "/")
+            
+            # check if a checkbutton does not yet exist AND if the file 
+            # is a csv (else you get an error)
+            if filename not in all_files and filename[-3:] == 'csv':
+                current_data = pd.read_csv(filename)
+                all_files[filename] = current_data
+                
+                
+                # create the filename for the checkbutton
+                last_i = len(filename) - 1
+                last = filename[last_i]
+                true_filename_length = 0
+                
+                while last != "/":
+                    true_filename_length += 1
+                    last_i -= 1
+                    last = filename[last_i]
+                
+                short_filename[filename] = filename[-true_filename_length:][:-4]
+                
+                # send file name into the dictionary for all files
+                prepare_graph(filename)
+                
+                r[filename] = IntVar()
+
+                # create a checkbutton
+                checks[filename] = Checkbutton(frame, text = short_filename[filename], variable=r[filename], 
+                                               onvalue=1, offvalue=0)
+                checks[filename].deselect()
+                checks[filename].pack()
+
 
 # select a file and create a check button for it
-def open_excel():
+def open_csv():
     filenames = filedialog.askopenfilenames(initialdir="/User/Windows 10/Desktop", title="select csv")
     
     for filename in filenames:
@@ -130,22 +174,28 @@ def make_graph():
         g_name = 'Water Flux Comparisons'
     graph_name.delete(0, END)
     
+    # colors...
+    unique_cats = set(category.values())
+    cycler = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
+    index = 0
+    for i in unique_cats:
+        category_hue[i]  = cycler[index]
+        index += 1
+        
+    #s
     plt.figure()
+    
     for i in all_files.keys():
         
         if r[i].get() == 1:
-        
-            k = graph_files[i].keys()
-            
-            #x = k[0]
-            #y = k[1]
-            
-            m = np.array(graph_files[i]['Weight'])
+
+            #make a np array so you can subtract from all values at once
+            m = np.array(graph_files[i]['Weight']) 
             m_0 = m[0]
             m_f = m - m_0
             
             plt.scatter(np.array(range(len(graph_files[i]['Time'])))*10, m_f, 
-                        label=short_filename[i])
+                        label=short_filename[i][:-5], c=category_hue[category[i]])
     
     # add 20LMH line
     #lmh_x = np.array(range(121)) * 10
@@ -225,6 +275,30 @@ def toggle_selection():
             
     toggler += 1
 
+# add letter
+def add_letter_function():
+    if not letter.get():
+        return
+    current_letter = letter.get()
+    letter.delete(0,END)
+    
+    for i in all_files.keys():
+        if r[i].get() == 1:
+            
+            if short_filename[i][-1] == '!':
+                short_filename[i] = short_filename[i][:-2] + current_letter + '!'
+            else:
+                short_filename[i] += '~~~' + current_letter + '!'
+                
+            checks[i].config(text=short_filename[i])
+            
+            category[i] = short_filename[i][-2]
+            
+# cut off any characters added after the first in the "letter" entry widget
+def limit_letter(*args):
+    value = letter.get()
+    if len(value) > 1: letter_value.set(value[:1])
+    
 
 # =============================================================================
 # ## -- WIDGETS -- ##
@@ -239,11 +313,13 @@ def toggle_selection():
 # ---- interactive input? --> BOX: (name, pressure) ... "add another"? 
 # - output --> max d of each 
 
-
+# create button that allows user to select directory
+dir_button = Button(frame, text="Select Directory of CSV's", command=open_dir)
+dir_button.pack()
 
 # create button that allows user to select file
-get_button = Button(frame, text="Select CSV's", command=open_excel)
-get_button.pack()
+csv_button = Button(frame, text="Select Individual CSV's", command=open_csv)
+csv_button.pack()
 
 # used for debugging
 show_all = Button(frame, text="Show all_files", command=show_all_files).pack()
@@ -251,7 +327,7 @@ show_all = Button(frame, text="Show all_files", command=show_all_files).pack()
 #graph name -- .get() to retrieve
 graph_name = Entry(frame, width=80)
 graph_name.pack()
-graph_name.insert(0, 'Enter graph\'s name')
+#graph_name.insert(0, 'Enter graph\'s name')
 
 # create button to graph
 make_graph_button = Button(frame, text="Make Graph", command=make_graph).pack()
@@ -263,6 +339,20 @@ csv_name.insert(0, 'Enter filename (without .csv)')
 
 # create button that shows flux of selected
 make_flux_button = Button(frame, text="Calculate J, Water Flux", command=show_flux).pack()
+
+# create bubble point diameter calculator -- create window that takes in data? excel file with all? separate tkinter program?
+
+
+#TEMP BUTTONS -- NO - PERMANENTYLY ADD A LETTER INFRONT OF THE CHECKBUTTON -- DICTIONARY THAT STORES TYPE -- category
+# add "letter"
+# add a trace on the value of the entry box that keeps only the first value 
+letter_value = StringVar()
+letter_value.trace('w', limit_letter) 
+letter = Entry(frame, width=60, textvariable=letter_value)
+letter.pack()
+
+add_letter = Button(frame,text='Add to {letter} category [PBS]', command=add_letter_function).pack()
+
 
 # make toggle button
 make_toggle_button= Button(frame, text="Select all", command=toggle_selection).pack()
